3df31b065a4a2450abcb140fed1671af4ffe2189
abdallah <abdallahantony55.aa@gmail.com>
2022-12-01 20:10:06 +0300

    refactor to hooks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Calculator from './components/Calculator';
 
-class App extends React.Component {
-  render() {
-    return (
+const App = () => (
     <div className="App">
      <Calculator />
     </div>
-    );
-  }
-}
+);
+
 export default App;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,93 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
+const Calculator = () => {
+  const [calculateObject, setCalculateObject] = useState({
+    total: null,
+    next: null,
+    operation: null,
 
-    this.state = {
-      calculateObject: {
-        total: null,
-        next: null,
-        operation: null,
+  });
 
-      },
-    };
-  }
-
-  handleClick=(e) => {
+  const handleClick = (e) => {
     const buttonName = e.target.innerText;
-    const obj = calculate(this.state.calculateObject, buttonName);
-    console.log(obj);
-    this.setState({ calculateObject: obj });
-  }
+    const obj = calculate(calculateObject, buttonName);
+    setCalculateObject(obj);
+  };
 
-  render() {
-    return <div className='calculator-wrapper'>
+  return <div className='calculator-wrapper'>
     <div className='screen'>
-        <p>{this.state.calculateObject.total}
-        {this.state.calculateObject.operation}
-        {this.state.calculateObject.next}</p>
+        <p>{calculateObject.total}
+        {calculateObject.operation}
+        {calculateObject.next}</p>
     </div>
     <div className='btn-container'>
      <div className='btn-row'>
 
-    <button className='btn' onClick={this.handleClick}>AC</button>
+    <button className='btn' onClick={ handleClick}>AC</button>
 
-    <button className='btn' onClick={this.handleClick}>+/-</button>
+    <button className='btn' onClick={ handleClick}>+/-</button>
 
-    <button className='btn' onClick={this.handleClick}>%</button>
+    <button className='btn' onClick={ handleClick}>%</button>
 
-    <button className='btn btn-last' onClick={this.handleClick}>&divide;</button>
-
-    </div>
-    <div className='btn-row'>
-
-    <button className='btn' onClick={this.handleClick}>7</button>
-
-    <button className='btn' onClick={this.handleClick}>8</button>
-
-    <button className='btn' onClick={this.handleClick}>9</button>
-
-    <button className='btn btn-last' onClick={this.handleClick}>x</button>
+    <button className='btn btn-last' onClick={ handleClick}>&divide;</button>
 
     </div>
     <div className='btn-row'>
 
-    <button className='btn' onClick={this.handleClick}>4</button>
+    <button className='btn' onClick={ handleClick}>7</button>
 
-    <button className='btn' onClick={this.handleClick}>5</button>
+    <button className='btn' onClick={ handleClick}>8</button>
 
-    <button className='btn'onClick={this.handleClick}>6</button>
+    <button className='btn' onClick={ handleClick}>9</button>
 
-    <button className='btn btn-last' onClick={this.handleClick}>-</button>
-
-    </div>
-    <div className='btn-row'>
-
-    <button className='btn' onClick={this.handleClick}>1</button>
-
-    <button className='btn' onClick={this.handleClick}>2</button>
-
-    <button className='btn' onClick={this.handleClick}>3</button>
-
-    <button className='btn btn-last' onClick={this.handleClick}>+</button>
+    <button className='btn btn-last' onClick={ handleClick}>x</button>
 
     </div>
     <div className='btn-row'>
 
-    <button className='btn btn-zero' onClick={this.handleClick}>0</button>
+    <button className='btn' onClick={ handleClick}>4</button>
 
-    <button className='btn' onClick={this.handleClick}>.</button>
+    <button className='btn' onClick={ handleClick}>5</button>
 
-    <button className='btn btn-last' onClick={this.handleClick}>=</button>
+    <button className='btn'onClick={ handleClick}>6</button>
+
+    <button className='btn btn-last' onClick={ handleClick}>-</button>
+
+    </div>
+    <div className='btn-row'>
+
+    <button className='btn' onClick={ handleClick}>1</button>
+
+    <button className='btn' onClick={ handleClick}>2</button>
+
+    <button className='btn' onClick={ handleClick}>3</button>
+
+    <button className='btn btn-last' onClick={ handleClick}>+</button>
+
+    </div>
+    <div className='btn-row'>
+
+    <button className='btn btn-zero' onClick={ handleClick}>0</button>
+
+    <button className='btn' onClick={ handleClick}>.</button>
+
+    <button className='btn btn-last' onClick={ handleClick}>=</button>
 
     </div>
 
     </div>
     </div>;
-  }
-}
+};
 
 export default Calculator;
