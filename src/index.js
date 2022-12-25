7e1329d91ccd075f9882280373b7932e1ef6e972
abdallah <abdallahantony55.aa@gmail.com>
2022-12-25 09:00:45 +0300

    try useContext hook

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { HashRouter as Router } from 'react-router-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const MyContext = createContext();
+
+const quote = {
+  name: 'William Paul Thurston',
+  description: "is not about numbers, equations,computations or algorithms; it's about understanding.”",
+};
+
 root.render(
   <React.StrictMode>
     <Router>
+      <MyContext.Provider value={quote}>
       <App />
+      </MyContext.Provider>
     </Router>
   </React.StrictMode>,
 );
+
+export default MyContext;
